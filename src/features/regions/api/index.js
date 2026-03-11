39195464a2b4d7a6a69310dd06b02820cac9c423
yaxyobekuz <pubgn9642@gmail.com>
2026-03-11 20:03:34 +0500

    feat(regions): add API for fetching all regions and region details by ID

---
+++ src/features/regions/api/index.js
@@ -0,0 +1,6 @@
+import http from "@/shared/api/http";
+
+export const regionsAPI = {
+  getAll: (params) => http.get("/api/regions", { params }),
+  getById: (id) => http.get(`/api/regions/${id}`),
+};
