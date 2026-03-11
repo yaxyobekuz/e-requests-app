873a17ffacfaf059ac768b3c314f04a5b7f7c535
yaxyobekuz <pubgn9642@gmail.com>
2026-03-11 19:59:25 +0500

    feat: add Axios instance and API exports for shared API module

---
+++ src/shared/api/index.js
@@ -0,0 +1,9 @@
+export { mskAPI } from "@/features/msk/api";
+export { authAPI } from "@/features/auth/api";
+export { regionsAPI } from "@/features/regions/api";
+export { settingsAPI } from "@/features/settings/api";
+export { requestsAPI } from "@/features/requests/api";
+export { adminRolesAPI } from "@/features/admin-roles/api";
+export { adminsAPI } from "@/features/admin-management/api";
+export { requestTypesAPI } from "@/features/request-types/api";
+export { servicesAPI, serviceReportsAPI } from "@/features/services/api";
